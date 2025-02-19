4301b028406c417a6cfe804b40866ef69ecdca95
sofia <g00471040@atu.ie>
2025-02-19 22:24:48 +0000

    testimg.py

---
+++ testimg.py
@@ -0,0 +1,28 @@
+#to create code we need to think about what blood pressure numbers mean and normal blood pressure for different stages of age 
+"""
+Blood pressure is recorded as two numbers, one over the other. Both are measured in units called millimetres of mercury(mm Hg)
+Systolic pressure: the top number, is the pressure each time your heart contracts (or squeezes)
+Diastolic pressure: the bottom number, is the pressure in the artery as the heart relaxes before the next beat.
+
+blood pressure    /     systolic     /     diastolic     /
+  low                 less than 90        less than 60
+  optimal             less than 120        less than 80
+  normal                 120-129              80-84
+  normal to high        130-139              85-89
+  high              greater than 140     greater than 90
+
+
+  Average blood pressure for teens and children:
+  1. Newborns up to 1 month: 60-90/ 20-60;
+  2. Infants: 87-105 / 53-66;
+  3. Toddlers: 95-105 / 53-66;
+  4. Preschoolers: 95-110 / 56-70;
+  5. School-aged children: 97-112 / 57-71;
+  6. Adolescents: 112-128 / 66-80;
+
+  Adulthood:
+  1. 18-39 years: women (110/68), men (119/70);
+  2. 40-59 years: women (122/74), men (124/77);
+  3. 60+ years: women (139/68), men (133/69);
+
+  """
